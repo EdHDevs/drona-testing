41bfcabcadf5f716fcc07a4b9c9d21bcf1d61d29
Edson <edson.h@vitasyahc.com>
2025-04-15 18:15:59 +0000

    App_2:: Minor app update Screen 'Screen 1' modified 'compactcard' control added in 'Screen 1' screen.

---
+++ App/App_2/screens/Screen 1/controls/main_control/compactcard/live_data.js
@@ -0,0 +1,32 @@
+[
+    {
+        "fullname": "Maria Gerhold",
+        "email": "maria81@example.com",
+        "avatar": "https://dronamobilepublic.s3.amazonaws.com/DRONA5_Team2050/content/app/images/public/Maria_T4j7j.jpg"
+    },
+    {
+        "fullname": "Fermin Schumm",
+        "email": "fermin.schumm51@example.com",
+        "avatar": "https://dronamobilepublic.s3.amazonaws.com/DRONA5_Team2050/content/app/images/public/Fermin_6ZIef.jpg"
+    },
+    {
+        "fullname": "Jalyn Labadie",
+        "email": "jalyn.labadie@example.com",
+        "avatar": "https://dronamobilepublic.s3.amazonaws.com/DRONA5_Team2050/content/app/images/public/Mask%20Group%204_SsFFA.jpg"
+    },
+    {
+        "fullname": "Garrick Wayne",
+        "email": "garrick77@example.com",
+        "avatar": "https://dronamobilepublic.s3.amazonaws.com/DRONA5_Team2050/content/app/images/public/Mask%20Group%204_SsFFA.jpg"
+    },
+    {
+        "fullname": "Candace Kunze",
+        "email": "candace.kunze@example.com",
+        "avatar": "https://dronamobilepublic.s3.amazonaws.com/DRONA5_Team2050/content/app/images/public/Mask%20Group%206_EA68w.jpg"
+    },
+    {
+        "fullname": "Salma Carroll",
+        "email": "salma.carroll@example.com",
+        "avatar": "https://dronamobilepublic.s3.amazonaws.com/DRONA5_Team2050/content/app/images/public/Mask%20Group%205_V8GqI.jpg"
+    }
+]
