0f7a8b3387127dd715c546d826b26eb7ed586328
Edson <edson.h@vitasyahc.com>
2025-04-16 19:22:10 +0000

    App_15:: Minor app update 'jsonviewer' control modified in 'Screen 1' screen.

--- App/App_15/screens/Screen 1/controls/main_control/jsonviewer/bind_data.js
+++ App/App_15/screens/Screen 1/controls/main_control/jsonviewer/bind_data.js
@@ -1,18 +1,1 @@
-[
-    {
-        "Month": "This is for wrapping example: Loen is a village in Stryn Municipality in Vestland county, Norway.",
-        "China": 21,
-        "Thiland": 5,
-        "India": 52,
-        "Bangladesh": 12,
-        "Combodia": 16
-    },
-    {
-        "Month": "Feb",
-        "China": 32,
-        "Thiland": 15,
-        "India": 52,
-        "Bangladesh": 12,
-        "Combodia": 16
-    }
-]
+{{json}}
