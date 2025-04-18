5baaa70b8b2775ba2472c3d1b109a41e99a802d0
Edson <edson.h@vitasyahc.com>
2025-04-18 12:39:42 +0000

    App_15:: Minor app update 'iconnavbar' control modified in 'Header 6' screen.

--- App/App_15/screens/Header 6/controls/header_control/iconnavbar/live_data.js
+++ App/App_15/screens/Header 6/controls/header_control/iconnavbar/live_data.js
@@ -11,31 +11,6 @@
         "icon": "fas fa-calendar"
     },
     {
-        "label": "Income",
-        "icon": "fas fa-wallet",
-        "options": [
-            {
-                "label": "Earnings",
-                "value": "Earnings",
-                "isActive": true
-            },
-            {
-                "label": "Refunds",
-                "value": "Refunds"
-            },
-            {
-                "label": "Declines",
-                "value": "Declines",
-                "description": ""
-            },
-            {
-                "label": "Payouts",
-                "value": "Payouts",
-                "description": ""
-            }
-        ]
-    },
-    {
         "label": "Posts",
         "value": "Posts",
         "icon": "fas fa-image",
