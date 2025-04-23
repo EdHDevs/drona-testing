63b4218a78d6cd97f1d0d412982983f9282daa9b
Edson <edson.h@vitasyahc.com>
2025-04-23 12:31:58 +0000

    App_18:: Minor app update 'container > dashboard1' control modified in 'Screen 1' screen.

--- App/App_18/screens/Screen 1/controls/main_control/container/controls/dashboard1/bind_data.js
+++ App/App_18/screens/Screen 1/controls/main_control/container/controls/dashboard1/bind_data.js
@@ -3,5 +3,5 @@
     "header_text": "Total Campaigns",
     "title_text": "$32,62.7",
     "footer_text": "335 Orders",
-    "tag_text": "12.5%"
+    "tag_text": ""
 }
--- App/App_18/screens/Screen 1/controls/main_control/container/controls/dashboard1/live_data.js
+++ App/App_18/screens/Screen 1/controls/main_control/container/controls/dashboard1/live_data.js
@@ -2,6 +2,5 @@
     "percentage": 20,
     "header_text": "Total Campaigns",
     "title_text": "$32,62.7",
-    "footer_text": "335 Orders",
-    "tag_text": "12.5%"
+    "footer_text": "335 Orders"
 }
