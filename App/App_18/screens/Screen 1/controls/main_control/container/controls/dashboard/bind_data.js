f3540d0b8c7be13a60dd051de38fd4898de72dbf
Edson <edson.h@vitasyahc.com>
2025-04-23 12:29:58 +0000

    App_18:: Minor app update 'container > dashboard' control deleted in 'Screen 1' screen. 'container > dashboard1' control modified in 'Screen 1' screen.

--- App/App_18/screens/Screen 1/controls/main_control/container/controls/dashboard/bind_data.js
+++
@@ -1,7 +0,0 @@
-{
-    "percentage": "",
-    "header_text": "Total Campaigns",
-    "title_text": "15",
-    "footer_text": "+2 from last month",
-    "tag_text": "Active"
-}
--- App/App_18/screens/Screen 1/controls/main_control/container/controls/dashboard/live_data.js
+++
@@ -1,6 +0,0 @@
-{
-    "header_text": "Total Campaigns",
-    "title_text": "15",
-    "footer_text": "+2 from last month",
-    "tag_text": "Active"
-}
