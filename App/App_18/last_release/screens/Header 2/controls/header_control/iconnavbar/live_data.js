88b4fc49db159c7ec087bad9ccbf718acf9c608c
Edson <edson.h@vitasyahc.com>
2025-04-23 14:45:59 +0000

    App_18:: App published

---
+++ App/App_18/last_release/screens/Header 2/controls/header_control/iconnavbar/live_data.js
@@ -0,0 +1,55 @@
+[
+    {
+        "label": "Dashboard",
+        "value": "Dashboard",
+        "icon": "fas fa-users",
+        "isActive": false,
+        "description": "",
+        "image": ""
+    },
+    {
+        "label": "Campaigns",
+        "value": "Campaigns",
+        "icon": "fas fa-calendar",
+        "description": "",
+        "image": "",
+        "isActive": false
+    },
+    {
+        "label": "Audience Manager",
+        "icon": "fas fa-wallet",
+        "options": [
+            {
+                "label": "Earnings",
+                "value": "Earnings",
+                "isActive": true
+            },
+            {
+                "label": "Refunds",
+                "value": "Refunds"
+            },
+            {
+                "label": "Declines",
+                "value": "Declines",
+                "description": ""
+            },
+            {
+                "label": "Payouts",
+                "value": "Payouts",
+                "description": ""
+            }
+        ],
+        "value": "Audience Manager",
+        "description": "",
+        "image": "",
+        "isActive": false
+    },
+    {
+        "label": "Campaign Planner",
+        "value": "Campaign Planner",
+        "icon": "fas fa-image",
+        "image": "",
+        "description": "",
+        "isActive": false
+    }
+]
---
+++ App/App_18/last_release/screens/Menu 3/controls/main_control/collapsiblemenu/bind_data.js
@@ -0,0 +1,100 @@
+[
+    {
+        "label": "Dashboard",
+        "value": "Dashboard",
+        "icon": "icon-home",
+        "active": true,
+        "image": "",
+        "tag": {
+            "color": "#26bc6a",
+            "value": ""
+        }
+    },
+    {
+        "label": "Campaigns",
+        "value": "campaigns",
+        "icon": "icon-briefcase",
+        "children": [
+            {
+                "label": "Analytics",
+                "children": [
+                    {
+                        "label": "Data Analyst",
+                        "value": "data_analyst",
+                        "icon": "icon-cloud-upload"
+                    },
+                    {
+                        "label": "Project Analyst",
+                        "value": "project_analyst",
+                        "icon": "icon-grid",
+                        "image": "",
+                        "active": true,
+                        "tag": {
+                            "color": "#FF00F5",
+                            "value": "new"
+                        }
+                    },
+                    {
+                        "label": "Program Analyst",
+                        "value": "program_analyst",
+                        "icon": "icon-vector",
+                        "active": true
+                    }
+                ],
+                "value": "analytics",
+                "icon": "icon-graph",
+                "active": false,
+                "image": "",
+                "tag": {
+                    "color": "#26bc6a",
+                    "value": ""
+                }
+            }
+        ],
+        "active": false,
+        "image": "",
+        "tag": {
+            "color": "#26bc6a",
+            "value": ""
+        }
+    },
+    {
+        "label": "Recipients",
+        "value": "recipients",
+        "icon": "icon-cursor",
+        "active": false,
+        "image": "",
+        "tag": {
+            "color": "#26bc6a",
+            "value": ""
+        }
+    },
+    {
+        "label": "History",
+        "value": "history",
+        "icon": "icon-globe",
+        "image": "",
+        "children": [
+            {
+                "label": "Sevices",
+                "value": "sevices",
+                "icon": "icon-settings"
+            }
+        ],
+        "active": false,
+        "tag": {
+            "color": "#26bc6a",
+            "value": ""
+        }
+    },
+    {
+        "label": "User",
+        "value": "user",
+        "icon": "icon-user",
+        "image": "",
+        "tag": {
+            "color": "var(--DHQ-primary-color)",
+            "value": "3"
+        }
+    }
+]
