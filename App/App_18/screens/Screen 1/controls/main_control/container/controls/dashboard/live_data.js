7f0efa09cfcc60240ac550b0ee679e67be5c2e14
Edson <edson.h@vitasyahc.com>
2025-04-23 12:25:18 +0000

    App_18:: Minor app update 'container > dashboard' control modified in 'Screen 1' screen.

--- App/App_18/screens/Screen 1/controls/main_control/container/controls/dashboard/live_data.js
+++ App/App_18/screens/Screen 1/controls/main_control/container/controls/dashboard/live_data.js
@@ -1,6 +1,6 @@
 {
-    "percentage": "20",
-    "header_text": "Total Revenue",
+    "percentage": 20,
+    "header_text": "Total Campaigns",
     "title_text": "$32,62.7",
     "footer_text": "335 Orders",
     "tag_text": "12.5%"
