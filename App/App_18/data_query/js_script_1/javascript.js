08f36d78678cbc2bc16b8ec40e6a336ae0e3ee28
David Sallusti <davidsallusti@me.com>
2025-04-24 15:17:57 +0000

    App_18:: Minor app update Data Query 'js_script_1' modified. 'textblock' control added in 'Screen 6' screen. 'row3 > col1' control modified in 'Screen 6' screen. 'row3 > col1 > button1' control modified in 'Screen 6' screen. 'row3 > col1 > gps' control modified in 'Screen 6' screen. 'row3 > col1 > panel' control modified in 'Screen 6' screen. 'row3 > col1 > select' control modified in 'Screen 6' screen.

--- App/App_18/data_query/js_script_1/javascript.js
+++ App/App_18/data_query/js_script_1/javascript.js
@@ -1,35 +1,12 @@
-(async () => {
-  const { PDFDocument } = PDFLib;
-
-  // Fetch the blank I-9 PDF from USCIS
-  const existingPdfBytes = await fetch("https://www.uscis.gov/sites/default/files/document/forms/i-9.pdf")
-    .then(res => res.arrayBuffer());
-
-  // Load the PDF
-  const pdfDoc = await PDFDocument.load(existingPdfBytes);
-  const form = pdfDoc.getForm();
-
-  // Get known field names (some guesswork may be required based on PDF)
-  // You'll need to inspect this via Adobe Acrobat or PDF debugging tools
-  const fields = form.getFields();
-  fields.forEach(f => console.log(f.getName())); // For dev use: to inspect field names
-
-  try {
-    // Example: Fill some known field names
-    form.getTextField('Employee Last Name (Family Name)').setText('Sallusti');
-    form.getTextField('Employee First Name (Given Name)').setText('David');
-    form.getTextField('Middle Initial').setText('M');
-    form.getTextField('Date of Birth (mm/dd/yyyy)').setText('01/01/1990');
-  } catch (err) {
-    console.error('Field not found or error filling:', err);
+  function getLocation() {
+      return new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            resolve(position);
+            },(error) => {
+           reject(error);
+          }
+        );
+      });
   }
-
-  const pdfBytes = await pdfDoc.save();
-
-  // Trigger download in browser
-  const blob = new Blob([pdfBytes], { type: 'application/pdf' });
-  const link = document.createElement('a');
-  link.href = URL.createObjectURL(blob);
-  link.download = 'Filled_I-9_Form.pdf';
-  link.click();
-})();
+  output = await getLocation();
