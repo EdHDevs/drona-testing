265f68adf6d1ac15f486f5100d438ac3d89b27aa
David Sallusti <davidsallusti@me.com>
2025-04-28 13:35:08 +0000

    App_19:: Minor app update 'row > col > tabbedcontainer' control added in 'Screen 2' screen.

---
+++ App/App_19/screens/Screen 2/controls/main_control/row/controls/col/controls/tabbedcontainer/bind_data.js
@@ -0,0 +1,30 @@
+[
+    {
+        "label": "Home",
+        "value": "home",
+        "icon": "icon-home",
+        "active": true,
+        "image": ""
+    },
+    {
+        "label": "Features",
+        "value": "features",
+        "icon": "icon-organization",
+        "active": false,
+        "image": ""
+    },
+    {
+        "label": "Product",
+        "value": "product",
+        "icon": "icon-handbag",
+        "active": false,
+        "image": ""
+    },
+    {
+        "label": "Admin",
+        "value": "admin",
+        "icon": "icon-user-follow",
+        "active": false,
+        "image": ""
+    }
+]
